ba7883e34003bc334927cf5330de3e987f3020f2
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2022-09-10 21:20:44 +0530

    header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import Header from './component/layout/Header';
+import Footer from './component/layout/Header/Footer/Footer';
+import Header from './component/layout/Header/Header/Header';
+
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-       <Header/>
+      <Header/>
+      <hr style={{marginBottom:"550px"}}/>
+      <Footer/>
     </BrowserRouter>
     </div>
   );
---
+++ frontend/src/component/layout/Header/Header/Header.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import logo from "../../../images/blog-shopify.jpg";
+import search from "../../../images/search.png";
+import profile from "../../../images/profile.png";
+import cart from "../../../images/cart.png";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+function Header() {
+
+ 
+  return (
+    <div>
+      <nav class="navbarhead">
+        <div class="container-fluid ">
+          <span class="navbar-text">
+        </span>
+        </div>
+      </nav>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <div className="d-flex">
+            <Link to="/">
+              <img
+                src={logo}
+                 alt="navbarlogo"
+                className="logo"
+              />
+            </Link>
+        </div>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarNav">
+            
+            <ul className="navbar-nav">
+            <div className="options">
+              <li className="nav-item">
+                <Link to="/" className="nav-link mx-2" aria-current="page">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/product" className="nav-link mx-2" aria-current="page">
+                 Product
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact" className="nav-link mx-2" aria-current="page">
+                 Contact
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/about" className="nav-link mx-2" aria-current="page">
+                  About
+                </Link>
+              </li>
+              </div>
+              <div className="icons"></div>
+              <Link to="/search">
+              <img
+                src={search}
+                 alt="navbarlogo"
+                className="log"
+              />
+            </Link>
+            <Link to="/profile">
+              <img
+                src={profile}
+                 alt="navbarlogo"
+                className="log"
+              />
+            </Link>
+            <Link to="/cart">
+              <img
+                src={cart}
+                 alt="navbarlogo"
+                className="log"
+              />
+            </Link>
+             </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
