933ebb4ce1be01001f9de90e9981b31e42ba6abd
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2022-09-14 17:14:56 +0530

    product

--- frontend/src/component/layout/Home/Home.jsx
+++ frontend/src/component/layout/Home/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import {CgMouse} from "react-icons/cg"
+import "./Home.css";
 
 const Home = () => {
   return (
     <div>
-      Home
+     <div className="banner">
+      <p>Welcome to Shopify </p>
+      <h1>FIND AMEZING PRODUCTS BELOW </h1>
+
+      <a href="#container">
+        <button>
+          Scroll <CgMouse/> 
+        </button>
+      </a>
+    
+     </div>
+     <h2 className="homeheading">Featured Product</h2>
+
+     <div className="container" id='container'> </div>
     </div>
   )
 }
---
+++ frontend/src/component/layout/Home/Product.jsx
@@ -0,0 +1,13 @@
+
+
+import React from 'react'
+
+const product = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default product
