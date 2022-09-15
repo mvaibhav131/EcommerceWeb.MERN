6398218e7e4d7f2f31c6c82ad11a9c8bde49f88e
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2022-09-15 16:52:08 +0530

    products

--- frontend/src/component/layout/Home/Home.jsx
+++ frontend/src/component/layout/Home/Home.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import {CgMouse} from "react-icons/cg"
 import "./Home.css";
+import Products from './Product';
+
+const product={
+  name:"Blue Shirt",
+  images:[{url:"https://cdn.shopify.com/s/files/1/1650/5551/products/men-s-round-neck-plain-t-shirt-navy-blue-regular-fit-t-shirt-wolfattire-2549451063341_800x.jpg?v=1561008965"}],
+  price:"3000",
+  _id:"tshirts"
+};
 
 const Home = () => {
   return (
@@ -18,7 +26,18 @@ const Home = () => {
      </div>
      <h2 className="homeheading">Featured Product</h2>
 
-     <div className="container" id='container'> </div>
+     <div className="container" id='container'>
+               <Products product={product}/>
+               <Products product={product}/>
+               <Products product={product}/>
+               <Products product={product}/>
+               <Products product={product}/>
+               <Products product={product}/>
+               <Products product={product}/>
+               <Products product={product}/>
+               <Products product={product}/>
+               <Products product={product}/>
+       </div>
     </div>
   )
 }
--- frontend/src/component/layout/Home/Product.jsx
+++ frontend/src/component/layout/Home/Product.jsx
@@ -1,16 +1,32 @@
 
+import {Link} from "react-router-dom";
+import React from 'react';
+import ReactStars from "react-rating-stars-component";
+import "./Home.css"
 
-import React from 'react'
+const options = {
+  edit : false,
+  color : "rgba(20,20,20,0.1)",
+  activeColor: "tomato",
+  size:window.innerWidth < 600 ? 10 : 20,
+  value:3.5,
+  isHalf:true,
+};
 
-const product = () => {
+const Products = ({product}) => {
   return (
-    <div>
-      
-    </div>
+  <Link className="productCard" to={product._id}>
+  <img  src={product.images[0].url} alt="blue-tshirt" />
+  <p>{product.name}</p>
+  <div>
+    <ReactStars {...options}/> <span> (254 reviews)</span>
+  </div>
+  <span>₹ {product.price}</span>
+  </Link>
   )
 }
 
-export default product
+export default Products;
     
 
 
