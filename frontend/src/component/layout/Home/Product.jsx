8874f3c85b3d860591582635761967a15733136b
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2022-09-17 20:37:43 +0530

    product arrangements

--- frontend/src/component/layout/Home/Product.jsx
+++ frontend/src/component/layout/Home/Product.jsx
@@ -12,12 +12,12 @@ const Products = ({product}) => {
     color : "rgba(20,20,20,0.1)",
     activeColor: "tomato",
     size:window.innerWidth < 600 ? 10 : 20,
-    value:product.ratings,
+    value:product.rating,
     isHalf:true,
   };
   return (
   <Link className="productCard" to={product._id}>
-  <img  src={product.images.url} alt="blue-tshirt" />
+  <img  src={product.images[0].url} alt="blue-tshirt" />
   <p>{product.name}</p>
   <div>
     <ReactStars {...options}/> <span> ({product.noOfReviews})</span>
